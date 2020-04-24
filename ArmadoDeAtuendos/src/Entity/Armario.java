ec82f002f06f48475ede677f8cf0e7c35355cb90
GastiV <gnvidal25@gmail.com>
2020-04-23 22:54:24 -0300

    Armario conoce lista de prendas, puede agregar o quitar prendas de ella

--- ArmadoDeAtuendos/src/Entity/Armario.java
+++ ArmadoDeAtuendos/src/Entity/Armario.java
@@ -1,4 +1,25 @@
 package Entity;
 
+import java.util.List;
+
 public class Armario {
+    List<Prenda> Prendas;
+
+    Armario(List<Prenda> prendas){
+        this.Prendas = prendas;
+    }
+    public List<Prenda> getPrendas() {
+        return Prendas;
+    }
+
+    private void agregarElementoALista(List<Prenda> listaDePrendas,Prenda nuevaPrenda){
+        listaDePrendas.add(nuevaPrenda);
+    }
+    private void eliminarElementoDeLista(List<Prenda> listaDePrendas,Prenda nuevaPrenda){
+        listaDePrendas.remove(nuevaPrenda);
+    }
+    public void agregarPrenda(Prenda prenda){
+        this.agregarElementoALista(this.getPrendas(), prenda);
+    }
+
 }
